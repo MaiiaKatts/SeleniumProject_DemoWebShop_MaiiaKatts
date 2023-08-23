655e697fa29323c4638b3d87b2091f0ed0e5d230
MaiiaKatts <my_email@example.org>
2023-08-23 17:23:41 +0300

    change annotation in OpenDemoWebShopSiteTest class

--- src/test/java/com/ait/qa26/demoWebShop/maiiakatts/OpenDemoWebShopSiteTest.java
+++ src/test/java/com/ait/qa26/demoWebShop/maiiakatts/OpenDemoWebShopSiteTest.java
@@ -2,6 +2,7 @@ package com.ait.qa26.demoWebShop.maiiakatts;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -24,7 +25,7 @@ public class OpenDemoWebShopSiteTest {
     }
 
     //PostCondition: after => tearDown Method
-    @AfterTest
+    @AfterMethod
     public void tearDown() {
         //driver.close();
         driver.quit();
